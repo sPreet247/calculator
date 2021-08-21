f82988f503d9e815020c832869346de275c4cfa8
spreet247 <samre2472@gmail.com>
2021-08-21 16:29:13 -0400

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (arr) {
+  return multiply(arr.reduce((a, b) => a * b, 0));
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
